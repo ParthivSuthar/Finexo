1958745eae92f17182a2fc308b991392f36f5d40
Parthiv Suthar <myworkflow7070@gmail.com>
2024-07-10 09:37:16 +0530

    Finexo

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import { Link, NavLink } from "react-router-dom"
+
+function Header() {
+  return (
+    <header className="bg-gradient-to-r from-blue-700 to-slate-800">
+        <div className="flex h-12 items-center justify-between px-11">
+            <Link to="/" className="text-white text-xl" >
+              Finexo
+            </Link>
+            <div>
+              <ul className="flex gap-5">
+                <li>
+                  <NavLink to="/" className={({isActive}) => `text-white text-lg ${isActive ? "text-cyan-400" : "text-white"}`}>
+                      Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about" className={({isActive}) => `text-white text-lg ${isActive ? "text-cyan-400" : "text-white"}`}>
+                    About
+                  </NavLink>
+
+                </li>
+                <li>
+                  <NavLink to="/services" className={({isActive}) => `text-white text-lg ${isActive ? "text-cyan-400" : "text-white"}`}>
+                    Services
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/whyus" className={({isActive}) => `text-white text-lg ${isActive ? "text-cyan-400" : "text-white"}`}>
+                    Why us
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/team" className={({isActive}) => `text-white text-xl ${isActive ? "text-cyan-400" : "text-white"}`}>
+                    Team
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './components/Layout/Layout.jsx'
+import About from './components/About.jsx'
+import Services from './components/Services.jsx'
+import WhyUs from './components/WhyUs.jsx'
+import Team from './components/Team.jsx'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='home' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='services' element={<Services />} />
+      <Route path='whyus' element={<WhyUs />} />
+      <Route path='team' element={<Team />} />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
