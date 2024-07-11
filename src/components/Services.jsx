0a035b4136f60c9e7cfa52c14660fd98df603334
Parthiv Suthar <myworkflow7070@gmail.com>
2024-07-11 09:33:40 +0530

    add Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { services } from "../constants/data"
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className="p-11">
+      <div className="space-y-8">
+        <div className="text-center">
+          <h2 className="font-bold text-2xl">Our <span className="text-cyan-500">Services</span></h2>
+          <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration</p>
+        </div>
+        <div className="grid grid-cols-3 place-items-center">
+          {services.map((dets, index) => (
+            <div key={index} className="max-w-sm text-center space-y-4">
+              <img src={dets.image} alt="wallet" className="w-12 mx-auto" />
+              <h3>{dets.title}</h3>
+              <p>{dets.desc}</p>
+              <a href="#">Read More</a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/constants/data.js
@@ -0,0 +1,9 @@
+import walletimg from "../assets/s1.png"
+import storageimg from "../assets/s2.png"
+import supportimg from "../assets/s3.png"
+
+export const services = [
+    {image: walletimg, title: "CURRENCY WALLET", desc: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using"},
+    {image: storageimg, title: "SECURITY STORAGE", desc: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using"},
+    {image: supportimg, title: "EXPERT SUPPORT", desc: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using"},
+]
