1842b40763cea02ee467acdd223fc5edf2a4821e
Parthiv Suthar <myworkflow7070@gmail.com>
2024-07-12 09:00:46 +0530

    add Team section

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { teams } from "../constants/data"
+
 
 function Team() {
   return (
-    <div>Team</div>
+    <div className="bg-gradient-to-r from-blue-700 to-slate-800 p-7 space-y-12">
+      <h2 className="text-white font-bold text-2xl text-center">Our <span className="text-cyan-500">Team</span></h2>
+      <div className="grid grid-cols-4 place-items-center gap-4">
+        {teams.map((dets, index) => (
+          <div className="bg-slate-800 border h-full w-full rounded-lg p-4 space-y-1" key={index}>
+            <img src={dets.image} alt="img" className="mx-auto w-14 border-2 rounded-full" />
+            <h3 className="text-white text-lg pl-14">{dets.title}</h3>
+            <p className="text-slate-200 text-sm pl-14">{dets.job}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/constants/data.js
+++ src/constants/data.js
@@ -5,6 +5,10 @@ import w1 from "../assets/w1.png"
 import w2 from "../assets/w2.png"
 import w3 from "../assets/w3.png"
 import w4 from "../assets/w4.png"
+import joseph from "../assets/team-1.jpg"
+import nancy from "../assets/team-2.jpg"
+import earl from "../assets/team-3.jpg"
+import josephine from "../assets/team-4.jpg"
 
 export const services = [
     {image: walletimg, title: "CURRENCY WALLET", desc: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using"},
@@ -17,4 +21,12 @@ export const whyChoose = [
     {image: w2, title: "Secure Investment", desc: "Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa? In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus animi autem natus"},
     {image: w3, title: "Instant Trading", desc: "Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa? In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus animi autem natus"},
     {image: w4, title: "Happy Customers", desc: "Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa? In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus animi autem natus"},
+]
+
+
+export const teams = [
+    {image: joseph, title: "Joseph Brown", job: "Marketing Head"},
+    {image: nancy, title: "Nancy White", job: "Marketing Head"},
+    {image: earl, title: "Earl Martinez", job: "Marketing Head"},
+    {image: josephine, title: "Josephine Allard", job: "Marketing Head"},
 ]
