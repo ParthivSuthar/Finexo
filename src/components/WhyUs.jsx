919d79cfd756fbded966a86ef6597758025f6d5f
Parthiv Suthar <myworkflow7070@gmail.com>
2024-07-12 08:32:47 +0530

    add Why section

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { whyChoose } from "../constants/data"
 
 function WhyUs() {
   return (
-    <div>WhyUs</div>
+    <div className="p-7 space-y-7">
+      <h2 className="font-bold text-2xl text-center">Why Choose <span className="text-cyan-500">Us</span></h2>
+      <div className="flex flex-col w-full space-y-9">
+          {whyChoose.map((dets, index) => (
+            <div className="flex flex-col gap-3 items-center max-w-xl mx-auto" key={index}>
+              <img src={dets.image} alt="img" className="w-16 rounded-full border-fuchsia-600 p-2 border-4" />
+              <h3>{dets.title}</h3>
+              <p>{dets.desc}</p>
+            </div>
+          ))}
+      </div>
+    </div>
   )
 }
 
--- src/constants/data.js
+++ src/constants/data.js
@@ -1,9 +1,20 @@
 import walletimg from "../assets/s1.png"
 import storageimg from "../assets/s2.png"
 import supportimg from "../assets/s3.png"
+import w1 from "../assets/w1.png"
+import w2 from "../assets/w2.png"
+import w3 from "../assets/w3.png"
+import w4 from "../assets/w4.png"
 
 export const services = [
     {image: walletimg, title: "CURRENCY WALLET", desc: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using"},
     {image: storageimg, title: "SECURITY STORAGE", desc: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using"},
     {image: supportimg, title: "EXPERT SUPPORT", desc: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using"},
+]
+
+export const whyChoose = [
+    {image: w1, title: "Expert Management", desc: "Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa? In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus animi autem natus"},
+    {image: w2, title: "Secure Investment", desc: "Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa? In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus animi autem natus"},
+    {image: w3, title: "Instant Trading", desc: "Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa? In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus animi autem natus"},
+    {image: w4, title: "Happy Customers", desc: "Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa? In tempore voluptate ducimus officia id, aspernatur nihil. Tempore laborum nesciunt ut veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus animi autem natus"},
 ]
